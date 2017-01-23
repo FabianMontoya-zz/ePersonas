790269c5182747dfa6882a1fbdfb695e9b993f06
Fabian D. Montoya <fmontoya@sasif.com.co>
2017-01-23 15:45:02 -0500

    Etiquetas para Evitar Cache en navegador
    Se agregan las etiquetas <meta http-equiv="cache-control"
    content="no-cache" />
    <meta http-equiv="Pragma" content="no-cache" />
    <meta http-equiv="Expires" content="-1" />
    debjao de todas las etiquetas
    <title></title>
    
    para evitar que se almacene Cache
    
    adicional se agregan sentencias
    
    Response.Cache.SetCacheability(HttpCacheability.NoCache)
    Response.Cache.SetAllowResponseInBrowserHistory(False)
    
    En Sasif.Master.vb para esta misma cuestión

--- PanelSeguridad/Sasif.Master.vb
+++ PanelSeguridad/Sasif.Master.vb
@@ -2,7 +2,8 @@
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        Response.Cache.SetCacheability(HttpCacheability.NoCache)
+        Response.Cache.SetAllowResponseInBrowserHistory(False)
     End Sub
 
 End Class
