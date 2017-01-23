33387fbf011a3478641b1851a3be0016caad5d58
Fabian D. Montoya <fmontoya@sasif.com.co>
2017-01-23 15:52:19 -0500

    Etiquetas para No cache en Sasif.Master.vb

--- PanelSeguridad/Sasif.Master.vb
+++ PanelSeguridad/Sasif.Master.vb
@@ -4,6 +4,9 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Response.Cache.SetCacheability(HttpCacheability.NoCache)
         Response.Cache.SetAllowResponseInBrowserHistory(False)
+        Response.AddHeader("Pragma", "no-cache")
+        Response.AddHeader("cache-control", "private")
+        Response.CacheControl = "no-cache"
     End Sub
 
 End Class
