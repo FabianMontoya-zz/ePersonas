b5dc113d95ce394657db7e3fd5700fda638a52dd
Fabian D. Montoya <fmontoya@sasif.com.co>
2017-03-03 11:29:51 -0500

    Cambio a Descarga de Ejecutable
    Se crea página para redirección de descarga de archivo setup de la
    aplicación por problemas al intentar descargar directamente

---
+++ PanelSeguridad/Files_Dowload/DowloadEnroller.aspx.vb
@@ -0,0 +1,23 @@
+﻿Public Class DowloadEnroller
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        DescargarArchivo()
+    End Sub
+
+    Private Sub DescargarArchivo()
+        'Limpiamos la salida
+        Response.Clear()
+        'Con esto le decimos al browser que la salida sera descargable
+        Response.ContentType = "application/octet-stream"
+        'esta linea es opcional, en donde podemos cambiar el nombre del fichero a descargar (para que sea diferente al original)
+        Response.AddHeader("Content-Disposition", "attachment; filename=EnrollermentAPP.exe")
+        'Escribimos el fichero a enviar 
+        Response.WriteFile("EnrollerSetup.exe")
+        'volcamos el stream 
+        Response.Flush()
+        'Enviamos todo el encabezado ahora
+        Response.End()
+    End Sub
+
+End Class
